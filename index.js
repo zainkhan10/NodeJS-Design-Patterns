4616468b929e0c674877486ba5c09983c3c01547
Zain Khan <zain.khaan147@gmail.com>
2021-03-08 18:54:41 +0500

    Eve PayDay Update

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ var userFactory = require('./userFactory');
 var alex = userFactory("Alex Banks", 100);
 var eve = userFactory("Eve Porcello", 100, 'employee', "This and That");
 
+eve.payDay(100)
+
 console.log(alex.toString());
 console.log(eve.toString());
