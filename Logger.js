4c5185cfdae76343e3fe93194a2b5e4a5bf839bb
Zain Khan <zain.khaan147@gmail.com>
2021-03-08 17:49:12 +0500

    Created Instance for Logger Class

--- Logger.js
+++ Logger.js
@@ -14,4 +14,17 @@ class Logger {
   }
 }
 
-module.exports = Logger;
+class Singleton {
+    constructor(){
+        if(!Singleton.instance){
+            Singleton.instance = new Logger();
+        }
+    }
+
+    getInstance() {
+        return Singleton.instance;
+    }
+
+}
+
+module.exports = Singleton;
