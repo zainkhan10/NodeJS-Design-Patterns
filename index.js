7f7fb966a4e0e8d83eeb1380c266e3c5bfeed24a
Zain Khan <zain.khaan147@gmail.com>
2021-03-10 20:55:28 +0500

    Integrate Strategy Pattern

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-var Store = require("./Store");
-var Shopper = require("./Shopper");
-var Mall = require("./Mall");
+var logger = require("./Logger");
 
-var catAndThings = new Store("Cats & Things");
-var insAndOuts = new Store("Ins and Outs");
+logger.log("Hello World");
+logger.log("Hi World");
+logger.log("Yo World");
 
-var alex = new Shopper("Alex");
-var eve = new Shopper("Eve");
-var sharon = new Shopper("Sharon");
-var mike = new Shopper("Mike");
+logger.changeStrategy("none");
 
-var valleyMall = new Mall();
-
-catAndThings.subscribe(alex);
-catAndThings.subscribe(eve);
-catAndThings.subscribe(mike);
-catAndThings.subscribe(valleyMall);
-
-insAndOuts.subscribe(sharon);
-insAndOuts.subscribe(valleyMall);
-
-catAndThings.sale(20);
-insAndOuts.sale(50);
-
-console.log(valleyMall.sales);
+logger.log("Hello World");
+logger.log("Hi World");
+logger.log("Yo World");
