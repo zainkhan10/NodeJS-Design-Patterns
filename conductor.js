461f6c2189803ccdb1ad7a8ff60792148598ce1b
Zain Khan <zain.khaan147@gmail.com>
2021-03-10 18:58:19 +0500

    Intergrate Command Pattern

--- conductor.js
+++ conductor.js
@@ -1,9 +1,8 @@
 class Conductor {
-    run(command){
-        console.log(`Executing command : ${command.name}`)
-        command.execute()
-    }
-
+  run(command) {
+    console.log(`Executing command : ${command.name}`);
+    command.execute();
+  }
 }
 
 module.exports = new Conductor();
