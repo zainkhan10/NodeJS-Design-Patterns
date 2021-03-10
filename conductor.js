8de82835c623be21f070d40ae4b44c192906bcfc
Zain Khan <zain.khaan147@gmail.com>
2021-03-10 18:58:06 +0500

    Integrate Command Pattern

---
+++ conductor.js
@@ -0,0 +1,9 @@
+class Conductor {
+    run(command){
+        console.log(`Executing command : ${command.name}`)
+        command.execute()
+    }
+
+}
+
+module.exports = new Conductor();
--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-var Store = require('./Store');
-var inventory = require('./inventory.json');
+var conductor = require("./conductor");
+var { ExitCommand, CreateCommand } = require("./commands");
 
-var skiShop = new Store('Steep and deep', inventory);
+var { createInterface } = require("readline");
 
-var searchItem = 'wax';
-var results = skiShop.find(searchItem);
+var rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-console.log(results);
+console.log("create <fileName> <text> | exit");
+rl.prompt();
+
+rl.on("line", (input) => {
+  var [commandText, ...remaining] = input.split(" ");
+  var [fileName, ...fileText] = remaining;
+  var text = fileText.join(" ");
+
+  switch (commandText) {
+    case "exit":
+      conductor.run(new ExitCommand());
+      break;
+
+    case "create":
+      conductor.run(new CreateCommand(fileName, text));
+      break;
+
+    default:
+      console.log(`${commandText} not found!`);
+  }
+
+  rl.prompt();
+});
